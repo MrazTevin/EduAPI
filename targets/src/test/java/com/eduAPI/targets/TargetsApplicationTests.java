6c5ce9031f1efc300d9c075bcb51f82a7d897ad9
MrazTevin <millatevin@gmail.com>
2023-02-15 10:57:27 +0300

    m: add project starter files

---
+++ targets/src/test/java/com/eduAPI/targets/TargetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduAPI.targets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
